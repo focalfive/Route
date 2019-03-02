82cb4f37b73c45ae9e1e8b826fcbf2a99a33cb4d
pureye4u <pureye4u@gmail.com>
2019-03-03 02:30:49 +0900

    Update project settings

--- Route/Route/ViewController.swift
+++ Route/Route/ViewController.swift
@@ -1,25 +1,111 @@
 //
 //  ViewController.swift
-//  Route
+//  route
 //
-//  Created by 이재복 on 2016. 8. 12..
-//  Copyright © 2016년 slowslipper. All rights reserved.
+//  Created by pureye4u on 02/03/2019.
+//  Copyright © 2019 slowslipper. All rights reserved.
 //
 
 import UIKit
+import GoogleMaps
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate {
+    
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var mapView: GMSMapView!
+    var zoomLevel: Float = 15.0
+    let defaultLocation = CLLocation(latitude: -33.869405, longitude: 151.199)
+    let db = Firestore.firestore()
+    var ref: DocumentReference? = nil
+    var locations: [[String: Any]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.distanceFilter = 50
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
+        let camera = GMSCameraPosition.camera(withLatitude: defaultLocation.coordinate.latitude,
+                                              longitude: defaultLocation.coordinate.longitude,
+                                              zoom: zoomLevel)
+        mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
+        mapView.settings.myLocationButton = true
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.delegate = self
+        mapView.isMyLocationEnabled = true
+        
+        // Add the map to the view, hide it until we've got a location update.
+        view.addSubview(mapView)
+//        mapView.isHidden = true
+        
+        ref = db.collection("users").document("test02")
+//        db.collection("users").addSnapshotListener { documentSnapshot, error in
+        ref?.addSnapshotListener { documentSnapshot, error in
+            guard let document = documentSnapshot else {
+                print("Error fetching document: \(error!)")
+                return
+            }
+            print("Current data: \(document.data())")
+            if let locations = document.data()["locations"] as? [[String: Any]] {
+                self.locations = locations
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: CLLocationManagerDelegate
+    // Handle incoming location events.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location: CLLocation = locations.last!
+        print("Location: \(location)")
+        
+        self.locations.append([
+            "date": Date().timeIntervalSince1970,
+            "latitude": location.coordinate.latitude,
+            "longitude": location.coordinate.longitude
+        ])
+        ref?.updateData(["locations": self.locations])
+        
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                              longitude: location.coordinate.longitude,
+                                              zoom: zoomLevel)
+        
+        if mapView.isHidden {
+            mapView.isHidden = false
+            mapView.camera = camera
+        } else {
+            mapView.animate(to: camera)
+        }
     }
-
+    
+    // Handle authorization for the location manager.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted:
+            print("Location access was restricted.")
+        case .denied:
+            print("User denied access to location.")
+            // Display the map using the default location.
+            mapView.isHidden = false
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+            print("Location status is OK.")
+        }
+    }
+    
+    // Handle location manager errors.
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        print("Error: \(error)")
+    }
+    
 
 }
 
